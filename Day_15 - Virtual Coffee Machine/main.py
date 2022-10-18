c473d71da176b869d4d1b7c052f840da00b66968
ancrocha <ancrocha@gmail.com>
2022-10-17 21:27:57 -0400

    new project

---
+++ Day_15 - Virtual Coffee Machine/main.py
@@ -0,0 +1,46 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    "money": 0,
+}
+
+def main():
+    user_selection = input("What would you like? (espresso/latte/cappuccino):")
+    if user_selection == "off":
+        return
+    elif user_selection == "report":
+        for k, v in resources:
+            print(k)
+
+
+
+
+
+main()
