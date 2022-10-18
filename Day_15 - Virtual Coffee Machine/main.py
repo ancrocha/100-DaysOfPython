3f191966cc99635265601d263c74a1705d8c9a57
Andre Rocha <ancrocha@gmail.com>
2022-10-18 16:56:43 -0400

    commit

--- Day_15 - Virtual Coffee Machine/main.py
+++ Day_15 - Virtual Coffee Machine/main.py
@@ -1,3 +1,6 @@
+from multiprocessing.spawn import prepare
+
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -38,11 +41,37 @@ def check_resources(choice):
             print(f"Sorry there is not enough {item}")
 
 
+def collect_coins(choice):
+    global resources
+    print("Please insert coins.")
+    quarters = float(input("How many quarters?: ") or 0) * 0.25
+    dimes = float(input("How many dimes?: ") or 0) * 0.1
+    nickles = float(input("How many nickles?: ") or 0) * 0.05
+    pennies = float(input("How many pennies?: ") or 0) * 0.01
+
+    drink_price = float(MENU[choice]['cost'])
+    total_money = quarters + dimes + nickles + pennies
+
+    if total_money >= drink_price:
+        change = total_money - drink_price
+        print(f"Here is ${change} in change.")
+        resources.update({'money': drink_price})
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+
+def prepare_drink(choice):
+    print(choice)
+
 
 is_on = True
 
+
 while is_on == True:
     user_selection = input("What would you like? (espresso/latte/cappuccino):")
+
     if user_selection == "off":
         is_on = False
     elif user_selection == "report":
@@ -50,14 +79,13 @@ while is_on == True:
         print(f"Milk: {resources['milk']}ml")
         print(f"Coffee: {resources['coffee']}g")
         print(f"Money: ${resources['money']}")
-    elif user_selection == "espresso":
-        check_resources("espresso")
-    elif user_selection == "latte":
-        check_resources("latte")
-    elif user_selection == "cappuccino":
-        check_resources("cappuccino") 
     else:
-        print("your choice does not exist")     
-
-
-
+        if user_selection in MENU:
+            check_resources(user_selection)
+            paid = collect_coins(user_selection)
+            if paid:
+                prepare_drink(user_selection)
+            else:
+                prepare_drink("not proceed")
+        else:
+            print("your choice does not exist")
