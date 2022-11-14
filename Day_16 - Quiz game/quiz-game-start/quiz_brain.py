6b06e972b4ad9005339d985b0268ea6689fa6559
Andre Rocha <ancrocha@gmail.com>
2022-11-14 16:55:37 -0500

    new files

---
+++ Day_16 - Quiz game/quiz-game-start/quiz_brain.py
@@ -0,0 +1,33 @@
+class QuizBrain:
+    def __init__(self, list):
+        self.question_numer = 0
+        self.question_list = list
+        self.score = 0
+
+    def still_has_questions(self):
+
+        return self.question_numer < len(self.question_list)
+
+        # same as bellow
+        # if self.question_numer < len(self.question_list):
+        #     return True
+        # else:
+        #     return False
+
+    def next_question(self):
+        current_question = self.question_list[self.question_numer]
+        self.question_numer += 1
+        user_answer = input(
+            f"Q.{self.question_numer}: {current_question.text} (True/False) ")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_answer}")
+        print(f"Your current score is: {self.score}/{self.question_numer}")
+        print("\n")
