b2304fc38402c8340d2a71d551623f3e26b83922
Andre Rocha <ancrocha@gmail.com>
2022-11-23 12:19:49 -0500

    finished project

--- Day_20 - snake game/main.py
+++ Day_20 - snake game/main.py
@@ -29,10 +29,22 @@ while game_is_on:
 
     snake.move()
 
-    # Detect colidion with food.
+    # Detect colision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increse_score()
+
+    # Detect colision with the wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect colition with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
--- Day_20 - snake game/scoreboard.py
+++ Day_20 - snake game/scoreboard.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
 
 
 class Scoreboard(Turtle):
@@ -12,8 +14,12 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center",
-                   font=("Arial", 24, "normal"))
+        self.write(f"Score: {self.score}", align=ALIGNMENT,
+                   font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increse_score(self):
         self.score += 1
